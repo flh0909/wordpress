f3835236640e99a2f3c0f59bd64f0b720e7bbabd
lianghua.fu <lianghua.fu@2339.com>
2015-07-27 22:07:12 +0800

    调整

--- wp-config.php
+++ wp-config.php
@@ -15,19 +15,22 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'wordpress');
+//define('DB_NAME', 'wordpress');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'root');
+//define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', '123123');
+//define('DB_PASSWORD', '123123');
 
 /** MySQL主机 */
-define('DB_HOST', 'localhost');
+//define('DB_HOST', 'localhost');
+
+
+include(dirname(__FILE__) . '/wp-config/mysql.php');
 
 /** 创建数据表时默认的文字编码 */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** 数据库整理类型。如不确定请勿更改 */
 define('DB_COLLATE', '');
